0422a808e4ef765d6a1b1effbc54c221e6e18c27
Nicky Gerritsen <nickygerritsen@me.com>
2011-01-09 14:48:53 +0100

    Add Event::inTime()

--- event.cpp
+++ event.cpp
@@ -1,22 +1,37 @@
 #include "event.h"
 
+#include "scoreboard.h"
+#include "judgingevent.h"
+
 namespace DJ {
-	namespace Model {
-		Event::Event(QString id, QDateTime datetime, QObject *parent) : QObject(parent) {
-			this->id = id;
-			this->datetime = datetime;
-		}
+namespace Model {
+Event::Event(QString id, QDateTime datetime, QObject *parent) : QObject(parent) {
+	this->id = id;
+	this->datetime = datetime;
+}
 
-		EventType Event::getType() {
-			return UNKNOWN;
-		}
+EventType Event::getType() {
+	return UNKNOWN;
+}
 
-		QString Event::getId() {
-			return this->id;
-		}
+QString Event::getId() {
+	return this->id;
+}
 
-		QDateTime Event::getDateTime() {
-			return this->datetime;
-		}
-	} // namespace Model
+QDateTime Event::getDateTime() {
+	return this->datetime;
+}
+
+bool Event::inTime(Scoreboard *scoreboard) {
+	if (this->getType() == SUBMISSIONEVENT) {
+		return (this->getDateTime() >= scoreboard->getContest()->getStart() && this->getDateTime() <= scoreboard->getContest()->getEnd());
+	} else if (this->getType() == JUDGINGEVENT) {
+		JudgingEvent *judgingEvent = (JudgingEvent *)this;
+		Event *submissionEvent = judgingEvent->getSubmissionEvent();
+		return (submissionEvent->getDateTime() >= scoreboard->getContest()->getStart() && submissionEvent->getDateTime() <= scoreboard->getContest()->getEnd());
+	} else {
+		return false;
+	}
+}
+} // namespace Model
 } // namespace DJ
--- event.h
+++ event.h
@@ -5,27 +5,29 @@
 #include <QDateTime>
 
 namespace DJ {
-	namespace Model {
-		enum EventType {
-			UNKNOWN,
-			JUDGINGEVENT,
-			SUBMISSIONEVENT
-		};
+namespace Model {
+class Scoreboard;
+enum EventType {
+	UNKNOWN,
+	JUDGINGEVENT,
+	SUBMISSIONEVENT
+};
 
-		class Event : public QObject {
-			Q_OBJECT
-		public:
-			explicit Event(QString id, QDateTime datetime, QObject *parent = 0);
-			virtual QString toString() = 0;
-			virtual EventType getType();
-			QString getId();
-			QDateTime getDateTime();
+class Event : public QObject {
+	Q_OBJECT
+public:
+	explicit Event(QString id, QDateTime datetime, QObject *parent = 0);
+	virtual QString toString() = 0;
+	virtual EventType getType();
+	QString getId();
+	QDateTime getDateTime();
+	bool inTime(Scoreboard *scoreboard);
 
-		protected:
-			QString id;
-			QDateTime datetime;
-		};
-	} // namespace Model
+protected:
+	QString id;
+	QDateTime datetime;
+};
+} // namespace Model
 } // namespace DJ
 
 #endif // EVENT_H
