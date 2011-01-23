9d65fd7d4a8e21a623fbb4d96b65b90588a4567c
Nicky Gerritsen <nickygerritsen@me.com>
2011-01-23 12:08:04 +0100

    Remove branding image selection Will be moved to settings

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	QApplication::setOrganizationName("Nicky Gerritsen");
+	QApplication::setOrganizationDomain("nickygerritsen.github.com");
+	QApplication::setApplicationName("DOMjura");
+	QApplication::setApplicationVersion("1.0");
 
 	DJ::Controller::MainController mc;
 	mc.showMainWindow();
