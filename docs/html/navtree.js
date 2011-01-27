c14cd0aeff694a4b83fa965ad5f745a3db998757
Nicky Gerritsen <nickygerritsen@me.com>
2011-01-27 22:08:08 +0100

    Add docs

---
+++ docs/html/navtree.js
@@ -0,0 +1,360 @@
+var NAVTREE =
+[
+  [ "DOMjura", "index.html", [
+    [ "Class List", "annotated.html", [
+      [ "DJ::View::AboutDialog", "class_d_j_1_1_view_1_1_about_dialog.html", null ],
+      [ "DJ::Model::Affiliation", "class_d_j_1_1_model_1_1_affiliation.html", null ],
+      [ "DJ::Model::Category", "class_d_j_1_1_model_1_1_category.html", null ],
+      [ "DJ::View::ColorPair", "struct_d_j_1_1_view_1_1_color_pair.html", null ],
+      [ "DJ::Model::Contest", "class_d_j_1_1_model_1_1_contest.html", null ],
+      [ "DJ::Model::Event", "class_d_j_1_1_model_1_1_event.html", null ],
+      [ "DJ::Model::Events", "class_d_j_1_1_model_1_1_events.html", null ],
+      [ "DJ::View::GradientCache", "class_d_j_1_1_view_1_1_gradient_cache.html", null ],
+      [ "DJ::View::HeaderGraphicsItem", "class_d_j_1_1_view_1_1_header_graphics_item.html", null ],
+      [ "DJ::Model::JudgingEvent", "class_d_j_1_1_model_1_1_judging_event.html", null ],
+      [ "DJ::Model::Language", "class_d_j_1_1_model_1_1_language.html", null ],
+      [ "DJ::View::LegendaGraphicsItem", "class_d_j_1_1_view_1_1_legenda_graphics_item.html", null ],
+      [ "DJ::Controller::MainController", "class_d_j_1_1_controller_1_1_main_controller.html", null ],
+      [ "DJ::View::MainDialog", "class_d_j_1_1_view_1_1_main_dialog.html", null ],
+      [ "DJ::Model::Problem", "class_d_j_1_1_model_1_1_problem.html", null ],
+      [ "DJ::View::ProblemGraphicsItem", "class_d_j_1_1_view_1_1_problem_graphics_item.html", null ],
+      [ "DJ::Model::RankedProblem", "struct_d_j_1_1_model_1_1_ranked_problem.html", null ],
+      [ "DJ::Model::RankedTeam", "class_d_j_1_1_model_1_1_ranked_team.html", null ],
+      [ "DJ::Controller::ReadDataController", "class_d_j_1_1_controller_1_1_read_data_controller.html", null ],
+      [ "DJ::ResultProblem", "struct_d_j_1_1_result_problem.html", null ],
+      [ "DJ::View::ResultsWindow", "class_d_j_1_1_view_1_1_results_window.html", null ],
+      [ "DJ::ResultTeam", "struct_d_j_1_1_result_team.html", null ],
+      [ "DJ::Model::Scoreboard", "class_d_j_1_1_model_1_1_scoreboard.html", null ],
+      [ "DJ::View::SettingsDialog", "class_d_j_1_1_view_1_1_settings_dialog.html", null ],
+      [ "DJ::Controller::StandingsController", "class_d_j_1_1_controller_1_1_standings_controller.html", null ],
+      [ "DJ::Controller::StatsController", "class_d_j_1_1_controller_1_1_stats_controller.html", null ],
+      [ "DJ::View::StatsDialog", "class_d_j_1_1_view_1_1_stats_dialog.html", null ],
+      [ "DJ::Model::SubmissionEvent", "class_d_j_1_1_model_1_1_submission_event.html", null ],
+      [ "DJ::Model::Team", "class_d_j_1_1_model_1_1_team.html", null ],
+      [ "DJ::View::TeamGraphicsItem", "class_d_j_1_1_view_1_1_team_graphics_item.html", null ],
+      [ "DJ::View::WinnerGraphicsItem", "class_d_j_1_1_view_1_1_winner_graphics_item.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "DJ::View::AboutDialog", "class_d_j_1_1_view_1_1_about_dialog.html", null ],
+      [ "DJ::Model::Affiliation", "class_d_j_1_1_model_1_1_affiliation.html", null ],
+      [ "DJ::Model::Category", "class_d_j_1_1_model_1_1_category.html", null ],
+      [ "DJ::View::ColorPair", "struct_d_j_1_1_view_1_1_color_pair.html", null ],
+      [ "DJ::Model::Contest", "class_d_j_1_1_model_1_1_contest.html", null ],
+      [ "DJ::Model::Event", "class_d_j_1_1_model_1_1_event.html", [
+        [ "DJ::Model::JudgingEvent", "class_d_j_1_1_model_1_1_judging_event.html", null ],
+        [ "DJ::Model::SubmissionEvent", "class_d_j_1_1_model_1_1_submission_event.html", null ]
+      ] ],
+      [ "DJ::Model::Events", "class_d_j_1_1_model_1_1_events.html", null ],
+      [ "DJ::View::GradientCache", "class_d_j_1_1_view_1_1_gradient_cache.html", null ],
+      [ "DJ::View::HeaderGraphicsItem", "class_d_j_1_1_view_1_1_header_graphics_item.html", null ],
+      [ "DJ::Model::Language", "class_d_j_1_1_model_1_1_language.html", null ],
+      [ "DJ::View::LegendaGraphicsItem", "class_d_j_1_1_view_1_1_legenda_graphics_item.html", null ],
+      [ "DJ::Controller::MainController", "class_d_j_1_1_controller_1_1_main_controller.html", null ],
+      [ "DJ::View::MainDialog", "class_d_j_1_1_view_1_1_main_dialog.html", null ],
+      [ "DJ::Model::Problem", "class_d_j_1_1_model_1_1_problem.html", null ],
+      [ "DJ::View::ProblemGraphicsItem", "class_d_j_1_1_view_1_1_problem_graphics_item.html", null ],
+      [ "DJ::Model::RankedProblem", "struct_d_j_1_1_model_1_1_ranked_problem.html", null ],
+      [ "DJ::Model::RankedTeam", "class_d_j_1_1_model_1_1_ranked_team.html", null ],
+      [ "DJ::Controller::ReadDataController", "class_d_j_1_1_controller_1_1_read_data_controller.html", null ],
+      [ "DJ::ResultProblem", "struct_d_j_1_1_result_problem.html", null ],
+      [ "DJ::View::ResultsWindow", "class_d_j_1_1_view_1_1_results_window.html", null ],
+      [ "DJ::ResultTeam", "struct_d_j_1_1_result_team.html", null ],
+      [ "DJ::Model::Scoreboard", "class_d_j_1_1_model_1_1_scoreboard.html", null ],
+      [ "DJ::View::SettingsDialog", "class_d_j_1_1_view_1_1_settings_dialog.html", null ],
+      [ "DJ::Controller::StandingsController", "class_d_j_1_1_controller_1_1_standings_controller.html", null ],
+      [ "DJ::Controller::StatsController", "class_d_j_1_1_controller_1_1_stats_controller.html", null ],
+      [ "DJ::View::StatsDialog", "class_d_j_1_1_view_1_1_stats_dialog.html", null ],
+      [ "DJ::Model::Team", "class_d_j_1_1_model_1_1_team.html", null ],
+      [ "DJ::View::TeamGraphicsItem", "class_d_j_1_1_view_1_1_team_graphics_item.html", null ],
+      [ "DJ::View::WinnerGraphicsItem", "class_d_j_1_1_view_1_1_winner_graphics_item.html", null ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "Namespace List", "namespaces.html", [
+      [ "DJ", "namespace_d_j.html", null ],
+      [ "DJ::Controller", "namespace_d_j_1_1_controller.html", null ],
+      [ "DJ::Model", "namespace_d_j_1_1_model.html", null ],
+      [ "DJ::View", "namespace_d_j_1_1_view.html", null ],
+      [ "DJ::View::Ui", "namespace_d_j_1_1_view_1_1_ui.html", null ]
+    ] ],
+    [ "Namespace Members", "namespacemembers.html", null ],
+    [ "File List", "files.html", [
+      [ "aboutdialog.h", "aboutdialog_8h.html", null ],
+      [ "affiliation.h", "affiliation_8h.html", null ],
+      [ "category.h", "category_8h.html", null ],
+      [ "contest.h", "contest_8h.html", null ],
+      [ "defines.h", "defines_8h.html", null ],
+      [ "event.h", "event_8h.html", null ],
+      [ "events.h", "events_8h.html", null ],
+      [ "gradientcache.h", "gradientcache_8h.html", null ],
+      [ "headergraphicsitem.h", "headergraphicsitem_8h.html", null ],
+      [ "judgingevent.h", "judgingevent_8h.html", null ],
+      [ "language.h", "language_8h.html", null ],
+      [ "legendagraphicsitem.h", "legendagraphicsitem_8h.html", null ],
+      [ "main.cpp", "main_8cpp.html", null ],
+      [ "maincontroller.h", "maincontroller_8h.html", null ],
+      [ "maindialog.h", "maindialog_8h.html", null ],
+      [ "problem.h", "problem_8h.html", null ],
+      [ "problemgraphicsitem.h", "problemgraphicsitem_8h.html", null ],
+      [ "rankedteam.h", "rankedteam_8h.html", null ],
+      [ "readdatacontroller.h", "readdatacontroller_8h.html", null ],
+      [ "resultswindow.h", "resultswindow_8h.html", null ],
+      [ "scoreboard.h", "scoreboard_8h.html", null ],
+      [ "settingsdialog.h", "settingsdialog_8h.html", null ],
+      [ "standingscontroller.h", "standingscontroller_8h.html", null ],
+      [ "statscontroller.h", "statscontroller_8h.html", null ],
+      [ "statsdialog.h", "statsdialog_8h.html", null ],
+      [ "submissionevent.h", "submissionevent_8h.html", null ],
+      [ "team.h", "team_8h.html", null ],
+      [ "teamgraphicsitem.h", "teamgraphicsitem_8h.html", null ],
+      [ "winnergraphicsitem.h", "winnergraphicsitem_8h.html", null ]
+    ] ],
+    [ "File Members", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
